7cea8b8c9eec3814712c58f4bedd3120fe7e309d
yousef amr <102539850+yadragon2007@users.noreply.github.com>
2023-01-20 17:05:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+let span = document.querySelector("span");
+let birthDay = {
+  day: 10,
+  month: 3,
+};
+function run() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  let d = today.getDate();
+  let mo = today.getMonth();
+  let y = today.getFullYear();
+
+
+  if (birthDay.day == d && birthDay.month == mo+1 && m == 0 ) {
+    span.innerHTML = 'Happy birth day miro';
+
+    let permission = Notification.permission;
+    if (permission === "granted") {
+      showNotification();
+    } else if (permission === "default") {
+      requestAndShowPermission();
+    } else {
+      alert("Use normal alert");
+    }
+  }
+
+  setTimeout(run, 1000);
+}
+
+run()
+function requestAndShowPermission() {
+  Notification.requestPermission(function (permission) {
+    if (permission === "granted") {
+      showNotification();
+    }
+  });
+}
+function showNotification() {
+  //  if(document.visibilityState === "visible") {
+  //      return;
+  //  }
+  let title = "happy birth day miro";
+  let icon = "./294799010_444416197574675_6699428907001267822_n.jpg"; //this is a large image may take more time to show notifiction, replace with small size icon
+  let body =
+    "i will love you for ever becuse you are the best girl in the world";
+
+  let notification = new Notification(title, { body, icon });
+
+  notification.onclick = () => {
+    notification.close();
+    window.parent.focus();
+
+  };
+}
