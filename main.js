e114f672ceff22fdab500936465c5fc97b1dff89
yousef amr <102539850+yadragon2007@users.noreply.github.com>
2023-01-20 19:29:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+let span = document.querySelector("span");
+let birthDay = {
+  day: 10,
+  month: 3,
+};
+
+
+// get permission
+let permission = Notification.permission;
+if (permission === "granted") {
+} else if (permission === "default") {
+  requestAndShowPermission();
+} else {
+  requestAndShowPermission();
+}
+
+//send req
+function requestAndShowPermission() {
+  Notification.requestPermission(function (permission) {
+    if (permission !== "granted") {
+      requestAndShowPermission();
+    }
+  });
+}
+
+
+function run() {
+  const today = new Date();
+  let m = today.getMinutes();
+  let d = today.getDate();
+  let mo = today.getMonth();
+
+  if (birthDay.day == d && birthDay.month == mo + 1 && m == 0) {
+    span.innerHTML = "Happy birth day miro";
+    showNotification();
+  }
+
+  setTimeout(run, 1000);
+}
+
+run();
+
+//show Notification
+
+function showNotification() {
+  //  if(document.visibilityState === "visible") {
+  //      return;
+  //  }
+  let title = "happy birth day miro";
+  let icon = "./294799010_444416197574675_6699428907001267822_n.jpg"; //this is a large image may take more time to show notifiction, replace with small size icon
+  let body =
+    "i will love you for ever becuse you are the best girl in the world";
+
+  let notification = new Notification(title, { body, icon });
+
+  notification.onclick = () => {
+    notification.close();
+    window.parent.focus();
+  };
+}
